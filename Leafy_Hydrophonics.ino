34e3ae5aaf0367a3fbed32ea43d86948bbfd2bce
Toriqul_Islam <33224137+ToriqulTarek@users.noreply.github.com>
2022-11-25 20:33:49 +0600

    Add files via upload

--- Leafy_Hydrophonics.ino
+++ Leafy_Hydrophonics.ino
@@ -25,20 +25,20 @@
 #include <addons/RTDBHelper.h>
 
 /* 1. Define the WiFi credentials */
-#define WIFI_SSID "WIFI_AP"
-#define WIFI_PASSWORD "WIFI_PASSWORD"
+#define WIFI_SSID "ANTT SOFT_ROOM"
+#define WIFI_PASSWORD "AnttRoboticsLtd123"
 
 // For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
 
 /* 2. Define the API Key */
-#define API_KEY "API_KEY"
+#define API_KEY "AIzaSyDXwRiKw4t12HSsp4PRIYvr7gJ3RzXxWkw"
 
 /* 3. Define the RTDB URL */
-#define DATABASE_URL "URL" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_URL "https://leafy-database-control-system-default-rtdb.asia-southeast1.firebasedatabase.app/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 
 /* 4. Define the user Email and password that alreadey registerd or added in your project */
-#define USER_EMAIL "USER_EMAIL"
-#define USER_PASSWORD "USER_PASSWORD"
+#define USER_EMAIL "admin@gmail.com"
+#define USER_PASSWORD "admin12345"
 
 float ph;
 float tds;
@@ -159,41 +159,18 @@ void loop()
     
 
     bool bVal;
-    Serial.printf("Get bool ref... %s\n", Firebase.RTDB.getBool(&fbdo, F("/test/bool"), &bVal) ? bVal ? "true" : "false" : fbdo.errorReason().c_str());
+    Serial.printf("Get bool ref... %s\n", Firebase.RTDB.getBool(&fbdo, F("/command/LED"), &bVal) ? bVal ? "true" : "false" : fbdo.errorReason().c_str());
 
-    Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, F("/test/int"), count) ? "ok" : fbdo.errorReason().c_str());
+    Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, F("/command/LED"), random(0,1)) ? "ok" : fbdo.errorReason().c_str());
 
-    Serial.printf("Get int... %s\n", Firebase.RTDB.getInt(&fbdo, F("/test/int")) ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
 
     int iVal = 0;
-    Serial.printf("Get int ref... %s\n", Firebase.RTDB.getInt(&fbdo, F("/test/int"), &iVal) ? String(iVal).c_str() : fbdo.errorReason().c_str());
+    Serial.printf("Get int ref... %s\n", Firebase.RTDB.getInt(&fbdo, F("/data/Humidity"), &iVal) ? String(iVal).c_str() : fbdo.errorReason().c_str());
 
-    Serial.printf("Set float... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/test/float"), count + 10.2) ? "ok" : fbdo.errorReason().c_str());
+    Serial.printf("Set float... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/data/Humidity"), count + 10.2) ? "ok" : fbdo.errorReason().c_str());
+    Serial.printf("Set double... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/data/Temperature"), count + 35.517549723765) ? "ok" : fbdo.errorReason().c_str());
 
-    Serial.printf("Get float... %s\n", Firebase.RTDB.getFloat(&fbdo, F("/test/float")) ? String(fbdo.to<float>()).c_str() : fbdo.errorReason().c_str());
-
-    Serial.printf("Set double... %s\n", Firebase.RTDB.setDouble(&fbdo, F("/test/double"), count + 35.517549723765) ? "ok" : fbdo.errorReason().c_str());
-
-    Serial.printf("Get double... %s\n", Firebase.RTDB.getDouble(&fbdo, F("/test/double")) ? String(fbdo.to<double>()).c_str() : fbdo.errorReason().c_str());
-
-    Serial.printf("Set string... %s\n", Firebase.RTDB.setString(&fbdo, F("/test/string"), F("Hello World!")) ? "ok" : fbdo.errorReason().c_str());
-
-    Serial.printf("Get string... %s\n", Firebase.RTDB.getString(&fbdo, F("/test/string")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
-
-    // For the usage of FirebaseJson, see examples/FirebaseJson/BasicUsage/Create_Edit_Parse.ino
-    FirebaseJson json;
-
-    if (count == 0)
-    {
-      json.set("value/round/" + String(count), F("cool!"));
-      json.set(F("value/ts/.sv"), F("timestamp"));
-      Serial.printf("Set json... %s\n", Firebase.RTDB.set(&fbdo, F("/test/json"), &json) ? "ok" : fbdo.errorReason().c_str());
-    }
-    else
-    {
-      json.add(String(count), F("smart!"));
-      Serial.printf("Update node... %s\n", Firebase.RTDB.updateNode(&fbdo, F("/test/json/value/round"), &json) ? "ok" : fbdo.errorReason().c_str());
-    }
+    
 
     Serial.println();
 
