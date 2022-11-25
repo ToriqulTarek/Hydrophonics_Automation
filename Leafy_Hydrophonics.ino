591c4427e24e10d70cd74e0e2f641bc945168d84
Toriqul_Islam <33224137+ToriqulTarek@users.noreply.github.com>
2022-11-26 01:54:07 +0600

    Add files via upload

--- Leafy_Hydrophonics.ino
+++ Leafy_Hydrophonics.ino
@@ -1,15 +1,4 @@
 
-/**
-   Created by K. Suwatchai (Mobizt)
-
-   Email: k_suwatchai@hotmail.com
-
-   Github: https://github.com/mobizt/Firebase-ESP-Client
-
-   Copyright (c) 2022 mobizt
-
-*/
-
 #if defined(ESP32)
 #include <WiFi.h>
 #elif defined(ESP8266)
@@ -27,8 +16,8 @@ DFRobot_DHT11 DHT;
 #include <addons/RTDBHelper.h>
 
 /* 1. Define the WiFi credentials */
-#define WIFI_SSID "ANTT SOFT_ROOM"
-#define WIFI_PASSWORD "AnttRoboticsLtd123"
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
 
 // For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
 
@@ -200,17 +189,6 @@ void tdsValue() {
       /* Assign the callback function for the long running token generation task */
       config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
 
-      // Or use legacy authenticate method
-      // config.database_url = DATABASE_URL;
-      // config.signer.tokens.legacy_token = "<database secret>";
-
-      // To connect without auth in Test Mode, see Authentications/TestMode/TestMode.ino
-
-      //////////////////////////////////////////////////////////////////////////////////////////////
-      // Please make sure the device free Heap is not lower than 80 k for ESP32 and 10 k for ESP8266,
-      // otherwise the SSL connection will fail.
-      //////////////////////////////////////////////////////////////////////////////////////////////
-
 #if defined(ESP8266)
       // In ESP8266 required for BearSSL rx/tx buffer for large data handle, increase Rx size as needed.
       fbdo.setBSSLBufferSize(2048 /* Rx buffer size in bytes from 512 - 16384 */, 2048 /* Tx buffer size in bytes from 512 - 16384 */);
@@ -227,36 +205,6 @@ void tdsValue() {
       Firebase.setDoubleDigits(5);
 
       config.timeout.serverResponse = 10 * 1000;
-
-      /** Timeout options.
-
-        //WiFi reconnect timeout (interval) in ms (10 sec - 5 min) when WiFi disconnected.
-        config.timeout.wifiReconnect = 10 * 1000;
-
-        //Socket connection and SSL handshake timeout in ms (1 sec - 1 min).
-        config.timeout.socketConnection = 10 * 1000;
-
-        //Server response read timeout in ms (1 sec - 1 min).
-        config.timeout.serverResponse = 10 * 1000;
-
-        //RTDB Stream keep-alive timeout in ms (20 sec - 2 min) when no server's keep-alive event data received.
-        config.timeout.rtdbKeepAlive = 45 * 1000;
-
-        //RTDB Stream reconnect timeout (interval) in ms (1 sec - 1 min) when RTDB Stream closed and want to resume.
-        config.timeout.rtdbStreamReconnect = 1 * 1000;
-
-        //RTDB Stream error notification timeout (interval) in ms (3 sec - 30 sec). It determines how often the readStream
-        //will return false (error) when it called repeatedly in loop.
-        config.timeout.rtdbStreamError = 3 * 1000;
-
-        Note:
-        The function that starting the new TCP session i.e. first time server connection or previous session was closed, the function won't exit until the
-        time of config.timeout.socketConnection.
-
-        You can also set the TCP data sending retry with
-        config.tcp_data_sending_retry = 1;
-
-      */
     }
 
     void loop()
@@ -275,102 +223,59 @@ void tdsValue() {
         temperature = DHT.temperature;
         humidity = DHT.humidity;
 
-        if (ph <= random(0,5)){
+        bool autoCommand = Firebase.RTDB.getBool(&fbdo, F("/command/autoMode"), &autoCommand) ? autoCommand ? "true" : "false" : fbdo.errorReason().c_str();
+        
+        while ((ph <= random(0,5)) || (autoCommand == true)){
+
+          unsigned long time = millis();
+        
           digitalWrite(realy1,LOW);
           digitalWrite(realy2,LOW);
           digitalWrite(realy3,LOW);
           digitalWrite(realy4,LOW);
+          if((time==10000) || (autoCommand == false)){
+
+          digitalWrite(realy1,HIGH);
+          digitalWrite(realy2,HIGH);
+          digitalWrite(realy3,HIGH);
+          digitalWrite(realy4,HIGH);
+          
+            break;
+          }
         }
 
 
 
-        bool bVal;
-        Serial.printf("Get bool ref... %s\n", Firebase.RTDB.getBool(&fbdo, F("/command/LED"), &bVal) ? bVal ? "true" : "false" : fbdo.errorReason().c_str());
+        
+        bool command = Firebase.RTDB.getBool(&fbdo, F("/command/LED"), &command) ? command ? "true" : "false" : fbdo.errorReason().c_str();
+        
+        if (command == true){
+          digitalWrite(LED,LOW);
+        }
+        else if (command == false){
+          digitalWrite(LED,HIGH);
+        }
 
-        Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, F("/command/LED"), random(0, 1)) ? "ok" : fbdo.errorReason().c_str());       
-        Serial.printf("Set float... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/data/Humidity"), humidity) ? "ok" : fbdo.errorReason().c_str());
-        Serial.printf("Set double... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/data/Temperature"), temperature) ? "ok" : fbdo.errorReason().c_str());
+        bool acLoad = Firebase.RTDB.getBool(&fbdo, F("/command/acLoad"), &acLoad) ? acLoad ? "true" : "false" : fbdo.errorReason().c_str();
+        if ( acLoad == true ){
 
-
+          digitalWrite(AC_LOAD,LOW);
+        }
+        else if (acLoad == false){
+          digitalWrite(AC_LOAD,HIGH);
+        }
+       
+        Serial.printf("Set humidity... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/data/Humidity"), humidity) ? "ok" : fbdo.errorReason().c_str());
+        Serial.printf("Set temperature... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/data/Temperature"), temperature) ? "ok" : fbdo.errorReason().c_str());
+        Serial.printf("Set ph... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/data/ph"), ph) ? "ok" : fbdo.errorReason().c_str());
+        Serial.printf("Set tds... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/data/tdsValue"), tds) ? "ok" : fbdo.errorReason().c_str());
 
         Serial.println();
 
-        // For generic set/get functions.
 
-        // For generic set, use Firebase.RTDB.set(&fbdo, <path>, <any variable or value>)
 
-        // For generic get, use Firebase.RTDB.get(&fbdo, <path>).
-        // And check its type with fbdo.dataType() or fbdo.dataTypeEnum() and
-        // cast the value from it e.g. fbdo.to<int>(), fbdo.to<std::string>().
-
-        // The function, fbdo.dataType() returns types String e.g. string, boolean,
-        // int, float, double, json, array, blob, file and null.
-
-        // The function, fbdo.dataTypeEnum() returns type enum (number) e.g. fb_esp_rtdb_data_type_null (1),
-        // fb_esp_rtdb_data_type_integer, fb_esp_rtdb_data_type_float, fb_esp_rtdb_data_type_double,
-        // fb_esp_rtdb_data_type_boolean, fb_esp_rtdb_data_type_string, fb_esp_rtdb_data_type_json,
-        // fb_esp_rtdb_data_type_array, fb_esp_rtdb_data_type_blob, and fb_esp_rtdb_data_type_file (10)
-
-        count++;
+        
       }
     }
 
-    /** NOTE:
-       When you trying to get boolean, integer and floating point number using getXXX from string, json
-       and array that stored on the database, the value will not set (unchanged) in the
-       FirebaseData object because of the request and data response type are mismatched.
-
-       There is no error reported in this case, until you set this option to true
-       config.rtdb.data_type_stricted = true;
-
-       In the case of unknown type of data to be retrieved, please use generic get function and cast its value to desired type like this
-
-       Firebase.RTDB.get(&fbdo, "/path/to/node");
-
-       float value = fbdo.to<float>();
-       String str = fbdo.to<String>();
-
-    */
-
-    /// PLEASE AVOID THIS ////
-
-    // Please avoid the following inappropriate and inefficient use cases
-    /**
-
-       1. Call get repeatedly inside the loop without the appropriate timing for execution provided e.g. millis() or conditional checking,
-       where delay should be avoided.
-
-       Everytime get was called, the request header need to be sent to server which its size depends on the authentication method used,
-       and costs your data usage.
-
-       Please use stream function instead for this use case.
-
-       2. Using the single FirebaseData object to call different type functions as above example without the appropriate
-       timing for execution provided in the loop i.e., repeatedly switching call between get and set functions.
-
-       In addition to costs the data usage, the delay will be involved as the session needs to be closed and opened too often
-       due to the HTTP method (GET, PUT, POST, PATCH and DELETE) was changed in the incoming request.
-
-
-       Please reduce the use of swithing calls by store the multiple values to the JSON object and store it once on the database.
-
-       Or calling continuously "set" or "setAsync" functions without "get" called in between, and calling get continuously without set
-       called in between.
-
-       If you needed to call arbitrary "get" and "set" based on condition or event, use another FirebaseData object to avoid the session
-       closing and reopening.
-
-       3. Use of delay or hidden delay or blocking operation to wait for hardware ready in the third party sensor libraries, together with stream functions e.g. Firebase.RTDB.readStream and fbdo.streamAvailable in the loop.
-
-       Please use non-blocking mode of sensor libraries (if available) or use millis instead of delay in your code.
-
-       4. Blocking the token generation process.
-
-       Let the authentication token generation to run without blocking, the following code MUST BE AVOIDED.
-
-       while (!Firebase.ready()) <---- Don't do this in while loop
-       {
-           delay(1000);
-       }
-
-    */
+    
